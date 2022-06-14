fa25bcb747f48c252d43b09250933320731234c6
Evan Jones <evanjones125@gmail.com>
2022-06-14 19:45:34 -0400

    solve reverseString

---
+++ recursion/reverseString.js
@@ -0,0 +1,6 @@
+const reverseString = (str) => {
+    if (str.length === 1) return str;
+    return str[str.length - 1] + reverseString(str.slice(0, str.length - 1));
+  }
+  
+  console.log(reverseString("kerhs"));
