055bcd7ca8cfeda999cbe9be975558b594b6330d
Evan Jones <evanjones125@gmail.com>
2022-06-23 13:10:59 -0400

    add functionality to exclude non-alphanumeric chars

--- charCount.js
+++ charCount.js
@@ -3,11 +3,8 @@ const charCount = str => {
     const store = {};
     const sorted = sortString(str);
 
-    for (let i in sorted) {
-        const char = sorted[i].toLowerCase();
-        if (store[char]) store[char] ++;
-        if (!store[char]) store[char] = 1;
-        // if (char === ' ') null;
+    for (let char of sorted) {
+        if (isAlphaNumeric(char)) store[char] = ++ store[char] || 1;
     }
 
     return store;
@@ -15,4 +12,14 @@ const charCount = str => {
 
 const sortString = text => text.split('').sort().join('');
 
-console.log(charCount("Testing"));
+const isAlphaNumeric = char => {
+    const code = char.charCodeAt(0);
+    if (!(code > 47 && code < 58) &&
+        !(code > 64 && code < 91) &&
+        !(code > 96 && code < 123)) {
+        return false
+    }
+    return true;
+}
+
+console.log(charCount("shrek is love!"));
