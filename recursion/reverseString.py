0b599efcea37685b9a9a19ef562508d8e8cf82b7
Evan Jones <evanjones125@gmail.com>
2022-06-27 20:26:56 -0400

    add new way to reverse string in python file

--- recursion/reverseString.py
+++ recursion/reverseString.py
@@ -1,6 +1,11 @@
-def reverse(str):
-    reversed = []
-    for i in range(len(str) - 1, -1, -1): reversed.append(str[i])
-    return reversed
+# reverse string using decrementing for loop
+# def reverse(str):
+#     reversed = []
+#     for i in range(len(str) - 1, -1, -1): reversed.append(str[i])
+#     return reversed
 
-print(reverse('shrek'))
+# reverse string using backwards slice
+def reverse(str):
+    return str[::-1]
+
+print(reverse('hello'))
