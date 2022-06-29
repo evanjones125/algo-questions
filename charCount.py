24b3a9ec2abc909ab135c33e36156f442bf3144b
Evan Jones <evanjones125@gmail.com>
2022-06-28 22:08:46 -0400

    create function and add pseudocode for charCount.py

---
+++ charCount.py
@@ -0,0 +1,11 @@
+# given a string, output a count of the incidence of each character
+def char_count(str):
+    output = {}
+
+    # iterate through str
+    # if output with str[i] label doesn't exist, create one
+    # if it does exist, increment it
+
+    return output
+
+print(char_count('shrek is love'))
--- pypractice.py
+++ pypractice.py
@@ -1,13 +1,20 @@
-# find smallest value in array
-from json.encoder import INFINITY
+# # find smallest value in array
+# from json.encoder import INFINITY
 
-arr = [3, 7, 2, 8, 5, -5, 6]
+# arr = [3, 7, 2, 8, 5, -5, 6]
 
-def find_smallest(array) :
-    smallest = INFINITY
-    for i in arr :
-        if i < smallest :
-            smallest = i
-    return smallest
+# def find_smallest(array) :
+#     smallest = INFINITY
+#     for i in arr :
+#         if i < smallest :
+#             smallest = i
+#     return smallest
 
-print(find_smallest(arr))
+# print(find_smallest(arr))
+
+smallest = None
+
+for i in [7, 8, 9, 64, 72]: 
+    if smallest is None or i < smallest: smallest = i
+
+print(smallest)
