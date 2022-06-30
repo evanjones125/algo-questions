cd82f1cfa35a8a234d8bc4fc67a741b6dfd23560
Evan Jones <evanjones125@gmail.com>
2022-06-30 18:51:38 -0400

    solve palindrome.py for inputs with no spaces/special chars

--- recursion/python/palindrome.py
+++ recursion/python/palindrome.py
@@ -1,7 +1,8 @@
 def palindrome(str):
-    if len(str) is 1: return True
+    if len(str) is 1 or 0: return True
     else:
         converted = list(str)
         if converted[0] is not converted[-1]: return False
+        else: return palindrome(converted[1:-1])
 
 print(palindrome('racecars'))
