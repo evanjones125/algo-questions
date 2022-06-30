940adf784151e0425205ea9df68d5ac4dc00cfbe
Evan Jones <evanjones125@gmail.com>
2022-06-30 18:49:44 -0400

    add list conversion and comparison in palindrome.py

--- recursion/python/palindrome.py
+++ recursion/python/palindrome.py
@@ -1,4 +1,7 @@
 def palindrome(str):
-    if len(str) is 1: return str
+    if len(str) is 1: return True
+    else:
+        converted = list(str)
+        if converted[0] is not converted[-1]: return False
 
-print(palindrome('r'))
+print(palindrome('racecars'))
