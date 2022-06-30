2f16cb3ff43e61fe740500a9f87b6128b3b961f5
Evan Jones <evanjones125@gmail.com>
2022-06-30 08:40:18 -0400

    add base and recursive cases

--- recursion/python/arrProduct.py
+++ recursion/python/arrProduct.py
@@ -1,8 +1,8 @@
 # take in array and return the sum of all its elements
 def arr_product(arr):
-    output = 0
-    for i in range(len(arr)):
-        output += arr[i]
-    return output
+    if len(arr) is 1: return arr[0]
+    else:
+        arr.pop()
+        return arr[-1] + arr_product(arr)
 
-print(arr_product([1, 4, 6, 2, 5]))
+print(arr_product([1, 3, 4]))
