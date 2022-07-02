164eb272347419b4eec203d405f3cef84880dae1
Evan Jones <evanjones125@gmail.com>
2022-07-01 22:03:00 -0400

    create separate file for palindrome_difficult

--- recursion/python/palindrome_difficult.py
+++ recursion/python/palindrome_difficult.py
@@ -2,7 +2,9 @@ def palindrome(str):
     if len(str) is 1 or 0: return True
     else:
         converted = list(str)
+        for i in converted:
+            if converted[i] == " ": converted.pop(converted[i])
         if converted[0] is not converted[-1]: return False
         else: return palindrome(converted[1:-1])
 
-print(palindrome('racecar'))
+print(palindrome('race car'))
