56e9c829df59f596219f594deea54c0bf2cbc022
Evan Jones <evanjones125@gmail.com>
2022-07-14 18:16:17 -0400

    add pseudocode to sumZero

---
+++ udemy course/sumZero.js
@@ -0,0 +1,10 @@
+// find the first pair where the sum is 0
+// return array that includes both values that sum to zero (or undefined)
+
+const sumZero = arr => {
+    // iterate through arr twice at the same time
+}
+
+console.log(sumZero([-3, -2, -1, 0, 1, 2, 3])); // [-3, 3]
+console.log(sumZero([-2, 0, 1, 3])); // undefined
+console.log(sumZero([1, 2, 3])); // undefined
