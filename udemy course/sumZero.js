5664e62ffa10dd9935bdc747ed767f2f79109495
Evan Jones <evanjones125@gmail.com>
2022-07-14 18:23:59 -0400

    solve sumZero with nested for loops

--- udemy course/sumZero.js
+++ udemy course/sumZero.js
@@ -3,6 +3,14 @@
 
 const sumZero = arr => {
     // iterate through arr twice at the same time
+    for (let i in arr) {
+        for (let j in arr) {
+            if (arr[i] !== 0 && arr[j] !== 0 && arr[i] + arr[j] === 0) {
+                return [arr[i], arr[j]];
+                //console.log(arr[i], arr[j]);
+            }
+        }
+    }
 }
 
 console.log(sumZero([-3, -2, -1, 0, 1, 2, 3])); // [-3, 3]
