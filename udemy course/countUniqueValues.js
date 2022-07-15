3cc762141160a3d70f269002a7e5232d4c36aa1d
Evan Jones <evanjones125@gmail.com>
2022-07-15 14:27:04 -0400

    update countUniqueValues to avoid undefineds

--- udemy course/countUniqueValues.js
+++ udemy course/countUniqueValues.js
@@ -1,14 +1,26 @@
 // count the unique values in an array
 
+// const countUniqueValues = arr => {
+//     let count = 0;
+
+//     for (let i = 0; i < arr.length; i++) {
+//         if (arr[i] !== arr[i + 1]) count ++;
+//     }
+
+//     return count;
+// }
+
 const countUniqueValues = arr => {
     let count = 0;
 
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] !== arr[i + 1]) count ++;
-        console.log(arr[i], arr[i + 1]);
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[count] !== arr[i]) {
+            count++;
+            arr[count] = arr[i];
+        }
     }
 
-    return count;
+    return count + 1;
 }
 
 console.log(countUniqueValues([1, 1, 1, 1, 1, 2])) // 2
