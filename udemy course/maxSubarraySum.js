eade5e0a4b48728d05df773f3a64c9929597e884
Evan Jones <evanjones125@gmail.com>
2022-07-16 14:56:48 -0400

    update maxSubarraySum with sliding window approach

--- udemy course/maxSubarraySum.js
+++ udemy course/maxSubarraySum.js
@@ -1,21 +1,36 @@
 // given an array and a number n, calculate the
 // maximum sum of n consecutive elements in the array
+
+// nested loop approach
+// const maxSubarraySum = (arr, n) => {
+//     if (!arr[0]) return null;
+//     if (n === 1) return Math.max(...arr);
+//     let output = 0;
+//     for (let i = 0; i < arr.length; i++) {
+//         let segment = 0;
+//         for (let j = i; j < n + i; j++) segment += arr[j];
+//         if (segment > output) output = segment;
+//     }
+//     return output;
+// }
+
+// sliding window approach
 const maxSubarraySum = (arr, n) => {
     if (!arr[0]) return null;
-    if (n === 1) return Math.max(...arr);
+    let maxSum = 0;
+    let segment = 0;
 
-    let output = 0;
-
-    for (let i = 0; i < arr.length; i++) {
-        let segment = 0;
-        for (let j = i; j < n + i; j++) {
-            segment += arr[j];
-        }
-
-        if (segment > output) output = segment;
+    for (let i = 0; i < n; i++) {
+        maxSum += arr[i];
     }
 
-    return output;
+    segment = maxSum;
+    for (let i = n; i < arr.length; i++) {
+        segment = segment - arr[i - n] + arr[i];
+        if (segment > maxSum) maxSum = segment;
+    }
+
+    return maxSum;
 }
 
 console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)) // 10
