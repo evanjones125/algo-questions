af1ee26bd524722626f60c7a2ef9453ad1fea8fe
Evan Jones <evanjones125@gmail.com>
2022-07-15 21:32:21 -0400

    create files for sequence and maxSubarraySum

---
+++ udemy course/maxSubarraySum.js
@@ -0,0 +1,11 @@
+// given an array and a number n, calculate the
+// maximum sum of n consecutive elements in the array
+const maxSubarraySum = (arr, n) => {
+
+}
+
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 2)) // 10
+console.log(maxSubarraySum([1, 2, 5, 2, 8, 1, 5], 4)) // 17
+console.log(maxSubarraySum([4, 2, 1, 6], 1)) // 6
+console.log(maxSubarraySum([4, 2, 1, 6, 2], 4)) // 13
+console.log(maxSubarraySum([], 4)) // null
