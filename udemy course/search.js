59266a8b7946680224fe8d91e63c36bc54040db5
Evan Jones <evanjones125@gmail.com>
2022-07-16 17:00:49 -0400

    solve search with naive solution

---
+++ udemy course/search.js
@@ -0,0 +1,13 @@
+// given a sorted array and a value n, return the index where n is located
+// if value is not found, return -1
+const search = (arr, n) => {
+    for (let i in arr) {
+        if (arr[i] === n) return i;
+    }
+
+    return -1;
+}
+
+console.log(search([1, 2, 3, 4, 5, 6], 4)) // 3
+console.log(search([1, 2, 3, 4, 5, 6], 6)) // 5
+console.log(search([1, 2, 3, 4, 5, 6], 11)) // -1
